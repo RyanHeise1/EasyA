63aca31717896bf4bf3e1caaacc1a0a36f273a83
aroskows <82058216+aroskows@users.noreply.github.com>
2023-01-25 15:19:00 -0800

    Add files via upload

---
+++ graphs/dataplay.py
@@ -0,0 +1,67 @@
+'''
+1/19/23 --> made an aper funct as proof of
+	concept 
+
+1/22 --> changed aper funct, so if same teacher
+	taught more than one class, the average
+	percent would be used
+
+
+'''
+
+import json
+import matplotlib.pyplot as plt
+
+f = open('gradedata.json', 'r')
+
+data = json.load(f)
+
+def aPer(class_name):
+	myDict = {}
+	#'instructor': [ average_aper, count] 
+	# 	eventually will probably want passing / d - f rate as well
+	for i in data[class_name]:
+		lname = i['instructor'].split(',')[0].strip()
+
+		if lname in myDict:
+			# we have a teacher who has taught already
+			# add to the count of classes taught
+			myDict[lname][1] += 1; 
+			# average the a precentages 
+			myDict[lname][0] = ( myDict[lname][0] + float(i['aprec']) ) / 2
+		else:
+			# first time this teacher has shown up (count is 1)
+			myDict[lname] = [float(i['aprec']), 1]
+
+	#create lists, so mathplots is easier		
+	instrucs = []
+	a_per = []
+
+	for i in myDict:
+		#add how many classes this professors done to name
+		instrucs.append(i + " (" + str(myDict[i][1]) + ")")
+		a_per.append(myDict[i][0])
+
+	#graphing 
+	fig, ax = plt.subplots()
+	ax.bar(instrucs, a_per)
+	ax.set_ylabel("Percentage of A's")
+	ax.set_xlabel("Professors' Last Names")
+	ax.set_title(class_name)
+	plt.show()
+
+	#print(myDict)
+
+
+aPer("AAAP510")
+aPer("AAAP511")
+aPer("AAD199")
+
+
+#What if bad input?! (not this problem)
+
+
+f.close()
+
+
+
