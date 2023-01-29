c0043f6f56157b0a7f1a3bd182a4102c0413ae0b
RyanHeise1 <62682418+RyanHeise1@users.noreply.github.com>
2023-01-29 11:39:32 -0800

    added d/f percentages into list. Fixed bug where it was not calculating average percentage correctly

--- graphs/dataplay.py
+++ graphs/dataplay.py
@@ -9,10 +9,13 @@
 1/25 --> code pushed to github
 
 1/26 --> added function to sort teacher data
+
+
 '''
 
 import json
 import matplotlib.pyplot as plt
+#from EasyA.parser.parser.py import *
 
 f = open('gd.js', 'r')
 
@@ -24,45 +27,104 @@ def aPer(class_name):
 	# 	eventually will probably want passing / d - f rate as well
 	for i in data[class_name]:
 		lname = i['instructor'].split(',')[0].strip()
+		total_failing = float(i['dprec']) + float(i['fprec'])
 
 		if lname in myDict:
 			# we have a teacher who has taught already
 			# add to the count of classes taught
-			myDict[lname][1] += 1; 
-			# average the a precentages 
-			myDict[lname][0] = ( myDict[lname][0] + float(i['aprec']) ) / 2
+			myDict[lname][2] += 1; 
+			myDict[lname][0] += float(i['aprec'])
+			myDict[lname][1] += total_failing
+
 		else:
 			# first time this teacher has shown up (count is 1)
-			myDict[lname] = [float(i['aprec']), 1]
-	myDict = sort_dict_by_value(myDict, key_func=lambda x: x[0])
+			#myDict[lname] = [float(i['aprec']), total_failing, 1]
+			myDict[lname] = [float(i['aprec']), total_failing, 1]
+
+	myDict = average_dict(myDict)
+	sorted_a = sort_dict_by_value(myDict, key_func=lambda x: x[0])
+	sorted_f = sort_dict_by_value(myDict, key_func=lambda x: x[1])
+	print(sorted_a)
+	print(sorted_f)
 
 	#create lists, so mathplots is easier		
-	instrucs = []
+	a_instrucs = []
 	a_per = []
+	f_instrucs = []
+	f_per = []
 
-	for i in myDict:
+	for i in sorted_a:
 		#add how many classes this professors done to name
-		instrucs.append(i + " (" + str(myDict[i][1]) + ")")
+		a_instrucs.append(i + " (" + str(myDict[i][2]) + ")")
 		a_per.append(myDict[i][0])
+	# instrucs = []
+	for j in sorted_f:
+		#instrucs.append(i + " (" + str(myDict[i][2]) + ")")
+		f_instrucs.append(j + " (" + str(myDict[j][2]) + ")")
+		f_per.append(myDict[j][1])
+	print(f'f_instrucs {f_instrucs}\nf_per {f_per}')
 
 
 	#graphing 
-	fig, ax = plt.subplots()
-	ax.bar(instrucs, a_per)
-	ax.set_ylabel("Percentage of A's")
-	ax.set_xlabel("Professors' Last Names")
-	ax.set_title(class_name)
-	plt.show()
+	display_d_f = True
+	if display_d_f == True:
+		ax = plt.subplot(1, 2, 1)
+		ax.barh(a_instrucs, a_per)
+		ax.set_xlabel("Percentage of A's")
+		ax.set_ylabel("Professors' Last Names")
+		ax.set_title(class_name)
+
+		ax = plt.subplot(1, 2, 2)
+		ax.barh(f_instrucs, f_per)
+		ax.set_xlabel("Percentage of D / F")
+		ax.set_ylabel("Professors' Last Names")
+		ax.set_title(class_name)
+		plt.show()
+		return
+	else:
+		fig, ax = plt.subplots()
+		ax.barh(a_instrucs, a_per)
+		ax.set_ylabel("Percentage of A's")
+		ax.set_xlabel("Professors' Last Names")
+		ax.set_title(class_name)
+		plt.show()
+		return
 
 	#print(myDict)
+
+
+"""
+Graphs needed
+	. Single Graph (Could be 2 if they want d/f percentages)
+		- Single class (MATH 111)
+		- Single Department (MATH)
+		- All classes in a department (All MATH 100-level)
+	. All classes of a particular level
+	. All instructors vs regular faculty
+		- all instructos are the default
+"""
+def average_dict(myDict):
+	for i in myDict:
+		total_classes = myDict[i][2]
+		# Average 'a' score
+		if myDict[i][0] != 0 or myDict[i][2] != 0:
+			myDict[i][0] = myDict[i][0] / total_classes
+		# Average 'd/f' score
+		if myDict[i][1] != 0 or myDict[i][2] != 0:
+			myDict[i][1] = myDict[i][1] / total_classes
+	return myDict
+
+def display_single_class(class_name, class_level, display_d_f=False):
+	return
+
 
 def sort_dict_by_value(d, key_func, reverse=True):
 	return dict(sorted(d.items(), key=lambda item: key_func(item[1]), reverse=reverse))
 
 
-aPer("AAAP510")
-aPer("AAAP511")
 aPer("AAD199")
+#aPer("AAAP511")
+#aPer("AAD199")
 
 
 #What if bad input?! (not this problem)
