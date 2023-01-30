73c8ccf3ebdea059fe860085b41b343c46577972
RyanHeise1 <62682418+RyanHeise1@users.noreply.github.com>
2023-01-29 19:54:39 -0800

    added graphs for specific department (ie All MATH 400-level classes)

--- graphs/dataplay2.py
+++ graphs/dataplay2.py
@@ -34,6 +34,7 @@ def A_percent(dep: str, level:str , classNum: str, allInstrucs: bool):
 		##	or just faculty; type = boolean 
 		##	(True is want all instuctor, False is just faculty)
 
+		# Parse all classes in department
 		if level == None and classNum == None:
 			#we only have department name, go through all the classes
 			d = p.parseGradeData(dep, None, None)
@@ -42,73 +43,180 @@ def A_percent(dep: str, level:str , classNum: str, allInstrucs: bool):
 				#plots each individual class
 				aPer(c, d[c])
 
+		# Parse level in department
 		elif level != None and classNum == None:
 			# we have the level of the department
 			allC = p.getClassNumbers(dep)
-			d = allC[int(level)]
-
-			print(d)
-
-			for c in d:
-				da = p.parseGradeData(dep, c, None)
-				#plots each individual class
-				aPer(dep + c, da)
-				pass
+			data = allC[int(level)]
+			department_graph(data, dep, level)
 
 
+		# Parse individual class
 		elif classNum != None:
 			# we have department and class number
 			#get data
 			d = p.parseGradeData(dep, classNum, None)
 			#plug into aPer
-			aPer(dep + classNum, d)
+			instructor_graph(dep + classNum, d)
+
+def department_graph(class_list, dep, level):
+	# {CLASS_NUM: [aperc, d+fperc, NUM_CLASSES]}
+	myDict = {}
+	for i in class_list:
+		class_data = p.parseGradeData(dep, i, None)
+		for j in class_data:
+			total_failing = float(j['dprec']) + float(j['fprec'])
+			aprec = float(j['aprec'])
+			if i in myDict:
+				myDict[i][0] += aprec # a percentage
+				myDict[i][1] += total_failing # d/f percentage
+				myDict[i][2] += 1 # number of classes
+			else:
+			 	myDict[i] = [aprec, total_failing, 1]
+	myDict = average_dict(myDict)
+	graph_data(myDict, "Classes", f'All {dep} {level}-level', True)
 
 
-
-def aPer(class_name, data):
+def instructor_graph(class_name, data):
 	myDict = {}
 	#'instructor': [ average_aper, count] 
 	# 	eventually will probably want passing / d - f rate as well
 	for i in data:
 		lname = i['instructor'].split(',')[0].strip()
+		total_failing = float(i['dprec']) + float(i['fprec'])
+		aprec = float(i['aprec'])
 
 		if lname in myDict:
 			# we have a teacher who has taught already
 			# add to the count of classes taught
-			myDict[lname][1] += 1; 
-			# average the a precentages 
-			myDict[lname][0] = ( myDict[lname][0] + float(i['aprec']) ) / 2
+			myDict[lname][2] += 1; 
+			myDict[lname][0] += aprec
+			myDict[lname][1] += total_failing
+
 		else:
 			# first time this teacher has shown up (count is 1)
-			myDict[lname] = [float(i['aprec']), 1]
+			#myDict[lname] = [float(i['aprec']), total_failing, 1]
+			myDict[lname] = [aprec, total_failing, 1]
+
+	myDict = average_dict(myDict)
+	sorted_a = sort_dict_by_value(myDict, key_func=lambda x: x[0])
+	sorted_f = sort_dict_by_value(myDict, key_func=lambda x: x[1])
 
 	#create lists, so mathplots is easier		
-	instrucs = []
+	a_instrucs = []
 	a_per = []
+	f_instrucs = []
+	f_per = []
 
-	for i in myDict:
+	for i in sorted_a:
 		#add how many classes this professors done to name
-		instrucs.append(i + " (" + str(myDict[i][1]) + ")")
+		a_instrucs.append(i + " (" + str(myDict[i][2]) + ")")
 		a_per.append(myDict[i][0])
+	# instrucs = []
+	for j in sorted_f:
+		#instrucs.append(i + " (" + str(myDict[i][2]) + ")")
+		f_instrucs.append(j + " (" + str(myDict[j][2]) + ")")
+		f_per.append(myDict[j][1])
 
 	#graphing 
-	fig, ax = plt.subplots(figsize = (12, 8))
-	ax.barh(instrucs, a_per)
-	ax.set_xlabel("Percentage of A's")
-	ax.set_ylabel("Professors' Last Names")
-	#ax.invert_yaxis()
-	ax.set_title(class_name)
-	plt.show()
+	display_d_f = False
+	if display_d_f == True:
+		ax = plt.subplot(1, 2, 1)
+		ax.barh(a_instrucs, a_per)
+		ax.set_xlabel("Percentage of A's")
+		ax.set_ylabel("Professors' Last Names")
+		ax.set_title(class_name)
+		ax.tick_params(axis='y', labelsize=6)
+
+		ax = plt.subplot(1, 2, 2)
+		ax.barh(f_instrucs, f_per)
+		ax.set_xlabel("Percentage of D / F")
+		ax.set_ylabel("Professors' Last Names")
+		ax.set_title(class_name)
+		ax.tick_params(axis='y', labelsize=6)
+		plt.tight_layout()
+		plt.show()
+		return
+	else:
+		fig, ax = plt.subplots()
+		ax.barh(a_instrucs, a_per)
+		ax.set_ylabel("Percentage of A's")
+		ax.set_xlabel("Professors' Last Names")
+		ax.set_title(class_name)
+		ax.tick_params(axis='y', labelsize=6)
+		plt.show()
+		return
+
+def graph_data(myDict, y_label: str, title:str, display_d_f: bool):
+	sorted_a = sort_dict_by_value(myDict, key_func=lambda x: x[0])
+	sorted_f = sort_dict_by_value(myDict, key_func=lambda x: x[1])
+
+	#create lists, so mathplots is easier		
+	a_data = []
+	a_per = []
+	f_data = []
+	f_per = []
+
+	for i in sorted_a:
+		#add how many classes this professors done to name
+		a_data.append(i + " (" + str(myDict[i][2]) + ")")
+		a_per.append(myDict[i][0])
+	# instrucs = []
+	for j in sorted_f:
+		#instrucs.append(i + " (" + str(myDict[i][2]) + ")")
+		f_data.append(j + " (" + str(myDict[j][2]) + ")")
+		f_per.append(myDict[j][1])
+
+	if display_d_f:
+		ax = plt.subplot(1, 2, 1)
+		ax.barh(a_data, a_per)
+		ax.set_xlabel("Percentage of A's")
+		ax.set_ylabel("Classes")
+		ax.set_title(title)
+		ax.tick_params(axis='y', labelsize=6)
+
+		ax = plt.subplot(1, 2, 2)
+		ax.barh(f_data, f_per)
+		ax.set_xlabel("Percentage of D / F")
+		ax.set_ylabel("Classes")
+		ax.set_title(title)
+		ax.tick_params(axis='y', labelsize=6)
+		plt.tight_layout()
+		plt.show()
+		return
+	else:
+		fig, ax = plt.subplots()
+		ax.barh(a_data, a_per)
+		ax.set_xlabel("Percentage of A's")
+		ax.set_ylabel("Classes")
+		ax.set_title(title)
+		ax.tick_params(axis='y', labelsize=6)
+		plt.show()
+	return
+
+def average_dict(myDict):
+	for i in myDict:
+		total_classes = myDict[i][2]
+		# Average 'a' score
+		if myDict[i][0] != 0 or myDict[i][2] != 0:
+			myDict[i][0] = myDict[i][0] / total_classes
+		# Average 'd/f' score
+		if myDict[i][1] != 0 or myDict[i][2] != 0:
+			myDict[i][1] = myDict[i][1] / total_classes
+	return myDict
 
 
-	#print(myDict)
+def sort_dict_by_value(d, key_func, reverse=True):
+	return dict(sorted(d.items(), key=lambda item: key_func(item[1]), reverse=reverse))
+
+
 
 
 #aPer("AAAP510")
 #aPer("AAAP511")
 #aPer("AAD199")
 
-#A_percent("BI", "200", None, True)
+A_percent("MATH", "100", None, True)
 #A_percent("MATH", None, "111", True)
 
 
