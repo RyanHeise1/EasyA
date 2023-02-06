1207835a2080230028da5a55c58156edfd02aef5
RyanHeise1 <62682418+RyanHeise1@users.noreply.github.com>
2023-02-05 16:52:53 -0800

    changed x-axis rotation to 90 after Zach found a bug with how the names are displayed

--- final/course_grade_visualizer.py
+++ final/course_grade_visualizer.py
@@ -317,7 +317,7 @@ def graph_data(myDict, x_label: str, title:str, display_d_f: bool, countClasses:
 			ax.set_xlabel(x_label)
 			ax.set_ylabel("Percentage of D / F")
 			ax.set_title(title)
-			ax.tick_params(axis='x', labelsize=10, rotation=45)
+			ax.tick_params(axis='x', labelsize=10, rotation=90)
 			# add height to the bars
 			rect = ax.patches
 			for rect, f_per in zip(rect, f_per):
@@ -358,7 +358,7 @@ def graph_data(myDict, x_label: str, title:str, display_d_f: bool, countClasses:
 			ax.set_xlabel(x_label)
 			ax.set_ylabel("Percentage of A's")
 			ax.set_title(title)
-			ax.tick_params(axis='x', labelsize=10, rotation=45)
+			ax.tick_params(axis='x', labelsize=10, rotation=90)
 			# add height to the bars
 			rect = ax.patches
 			for rect, a_per in zip(rect, a_per):
@@ -405,7 +405,7 @@ def graph_w_scroll(x, y, title, x_label, y_lable):
 		# https://www.geeksforgeeks.org/python-scroll-through-plots/
 	# Setting fig and ax variables as subplots()
 	fig, ax = plt.subplots(figsize=(10,6), num=title + " | " + y_lable)
-	ax.tick_params(axis='x', labelsize=10, rotation=45)
+	ax.tick_params(axis='x', labelsize=10, rotation=90)
 	ax.set_xlabel(x_label)
 	ax.set_ylabel(y_lable)
 	ax.set_title(title)
